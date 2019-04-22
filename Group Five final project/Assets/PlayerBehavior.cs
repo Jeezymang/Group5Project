bc27b3f30f4a733f995b8fda4590375e6a6a304c
isaiah-c <49883023+isaiah-c@users.noreply.github.com>
2019-04-22 15:06:27 -0500

    Unity Project

---
+++ Group Five final project/Assets/PlayerBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour {
+
+    public int speed = 3;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            Destroy (collision.gameObject);
+        }
+
+        else if (collision.gameObject.tag == "Foe")
+        {
+            Destroy(this.gameObject);
+            Destroy(collision.gameObject);
+        }
+        
+    }
+}
