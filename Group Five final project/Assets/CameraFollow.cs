011cd776161dd1d05bd2424071d3c688526ae1ba
Jon <Jeezymang@users.noreply.github.com>
2019-04-23 07:57:07 -0500

    Made changes to player movement and camera

--- Group Five final project/Assets/CameraFollow.cs
+++ Group Five final project/Assets/CameraFollow.cs
@@ -13,7 +13,7 @@ public class CameraFollow : MonoBehaviour {
     void Start()
     {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
+        offset = ((transform.position - player.transform.position).normalized) * 17;
     }
 
     // LateUpdate is called after Update each frame
@@ -23,6 +23,13 @@ public class CameraFollow : MonoBehaviour {
         if (player != null)
         {
             transform.position = player.transform.position + offset;
+            //transform.rotation = Quaternion.Euler(player.transform.rotation.eulerAngles - new Vector3(0, 35, 0));
+            //transform.LookAt(player.transform);
+            //transform.rotation = Quaternion.Euler(new Vector3(45, 45, 0));
+            Vector3 newCamPos = player.transform.position + (-player.transform.forward * 15/*follow distance*/);
+            newCamPos.y += 3; //FollowHeight
+            transform.position = newCamPos;
+            transform.LookAt(player.transform);
         }
     }
 }
--- Group Five final project/Assets/PlayerBehavior.cs
+++ Group Five final project/Assets/PlayerBehavior.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour {
-
+    
     public int speed = 3;
     public int phoneSpeed = 10;
+    int rotateSpeed = 2;
 	// Use this for initialization
 	void Start () {
 		
@@ -15,20 +16,35 @@ public class PlayerBehavior : MonoBehaviour {
 	void Update () {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += Vector3.left * speed * Time.deltaTime;
+            var eulerAngles = this.transform.eulerAngles;
+            var eulerQuat = Quaternion.Euler(eulerAngles.x, eulerAngles.y -= rotateSpeed, eulerAngles.z);
+            this.transform.rotation = eulerQuat;
+            //Gonna rotate the player instead
+            //transform.position += Vector3.left * speed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            var eulerAngles = this.transform.eulerAngles;
+            var eulerQuat = Quaternion.Euler(eulerAngles.x, eulerAngles.y += rotateSpeed, eulerAngles.z);
+            this.transform.rotation = eulerQuat;
+            //Gonna rotate the player instead
+            //transform.position += Vector3.right * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += Vector3.forward * speed * Time.deltaTime;
+            var rads = (this.transform.eulerAngles.y - 90) * (Mathf.PI / 180);
+            var plyDirection = new Vector3(Mathf.Cos(rads), 0, -Mathf.Sin(rads));
+            transform.position += (plyDirection * speed) * Time.deltaTime;
+            //This must be changed to move in the direction that the object is facing.
+            //transform.position += Vector3.forward * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += Vector3.back * speed * Time.deltaTime;
+            var rads = (this.transform.eulerAngles.y - 90) * (Mathf.PI / 180);
+            var plyDirection = new Vector3(-Mathf.Cos(rads), 0, Mathf.Sin(rads));
+            transform.position += new Vector3(Mathf.Abs(plyDirection.x * speed), Mathf.Abs(plyDirection.y * speed), Mathf.Abs(plyDirection.z * speed)) * -Time.deltaTime;
+            //transform.position += Vector3.back * speed * Time.deltaTime;
         }
         //For phone movement
         Vector3 direction = Vector3.zero;
