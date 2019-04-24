4653561498ed0cc62f9f76440266963660e2ff4a
isaiah-c <49883023+isaiah-c@users.noreply.github.com>
2019-04-24 10:40:55 -0500

    New thigns added.
    Added the new models, made the coin rotate and began work on the spawner.

---
+++ Group Five final project/Assets/Scripts/coinSPawn.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinSPawn : MonoBehaviour {
+    // A timer to countdown to the next coin spawn
+    public float timer = 5;
+    // Coin to be spawned
+    public GameObject coin;
+    // For testing purposes to make the spawner move.
+    public bool change = false;
+    public float changeTimer = 6;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //makes the timer count down.
+        timer -= Time.deltaTime;
+		
+        // When the timer hits zero instantiate the coin and reset the timer.
+        if (timer <= 0)
+        {
+            Instantiate(coin, transform.position,coin.transform.rotation);
+            timer = 5;
+        }
+
+
+        /* All below here is for testing purposes real version will be simply bounce off walls within the rooms.
+        A timer that determines the movement of the spawner counts down and when it hits zero.
+        it changes direction.
+        */
+    changeTimer -= Time.deltaTime;
+
+        if (changeTimer <= 0)
+        {
+            if (change == false)
+            {
+                change = true;
+            }
+            else change = false;
+
+            changeTimer = 6;
+        }
+
+        if (change == false)
+        {
+            transform.position += Vector3.left;
+        }
+        else transform.position += Vector3.right;
+
+	}
+}
---
+++ Group Five final project/Assets/Scripts/spin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour {
+
+    public int speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Makes the coin spin.
+        transform.Rotate(Vector3.forward, speed );
+	}
+}
