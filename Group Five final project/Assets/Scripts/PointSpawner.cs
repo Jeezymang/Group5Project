f8d8a42985dea4d21e547d3c895548c0f11b43d0
Jon <Jeezymang@users.noreply.github.com>
2019-05-01 03:44:55 -0500

    Added weighted random chances for gameobject spawns

--- Group Five final project/Assets/Scripts/PointSpawner.cs
+++ Group Five final project/Assets/Scripts/PointSpawner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +8,11 @@ public class PointSpawner : MonoBehaviour {
     public List<GameObject> SpawnPoints;
     public GameObject CoinPrefab;
     public GameObject EnemyPrefab;
+    public GameObject JumpPrefab;
+    public GameObject StarPrefab;
+    public GameObject SpeedupPrefab;
+    public GameObject DoubleCoinPrefab;
+
     float currentCountDownVal;
 	// Use this for initialization
 	void Start () {
@@ -24,20 +30,38 @@ public class PointSpawner : MonoBehaviour {
 
         //Spawn Logic
 
-        var rand = Random.Range(0, 4);
-        var transform = SpawnPoints[Random.Range(0, SpawnPoints.Count)].transform;
-        if (rand <= 1)
+        //var rand = Random.Range(0, 4);
+        var transform = SpawnPoints[UnityEngine.Random.Range(0, SpawnPoints.Count)].transform;
+        Dictionary<GameObject, int> spawnChances = new Dictionary<GameObject, int>
         {
-            
-            Instantiate(EnemyPrefab, transform.position, Quaternion.identity);
-            //Spawn Enemy
-        }
-        else if (rand > 1)
+            { CoinPrefab, 45 },
+            { SpeedupPrefab, 20 },
+            { EnemyPrefab, 40 },
+            { StarPrefab, 15 },
+            { JumpPrefab, 30 },
+            { DoubleCoinPrefab, 40 }
+        };
+        //Uses weighted chances
+        GameObject chosenPrefab = null;
+        var range = 0;
+        foreach(KeyValuePair<GameObject, int> pair in spawnChances)
         {
-            //Spawn Coin
-            Instantiate(CoinPrefab, transform.position + new Vector3(0, 1, 0), CoinPrefab.transform.rotation);
+            range += pair.Value;
         }
 
+        var rand = UnityEngine.Random.Range(0, range);
+        int top = 1;
+        foreach (KeyValuePair<GameObject, int> pair in spawnChances)
+        {
+            top = top + pair.Value;
+            if (rand < top)
+            {
+                chosenPrefab = pair.Key;
+                break;
+            }
+        }
+        
+        Instantiate(chosenPrefab, transform.position, Quaternion.identity);
         //Rerun routine
         StartCoroutine(Spawn(delay));
     }
